8b06258459f831ac60f399a8504b2791e7e3bce8
MmmarRTHa <m27842n@outlook.com>
2024-05-19 23:15:45 -0700

    - update: feature/expense_list_component - add UI

---
+++ src/components/ExpenseDetail.tsx
@@ -0,0 +1,33 @@
+import { useMemo } from 'react'
+import { formatDate } from '../helpers'
+import { Expense } from '../types'
+import AmountDisplay from './AmountDisplay'
+import { categories } from '../data/categories'
+
+type ExpenseDetailProps = {
+    expense : Expense
+}
+
+export default function ExpenseDetail({expense} : ExpenseDetailProps) {
+    const categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category)[0], [expense])
+
+    return (
+        <div className="flex items-center w-11/12 gap-5 p-10 mx-auto border-b rounded-sm shadow md:max-w-3xl bg-gradient-to-r from-gray-200 to-gray-300 shadow-indigo-300">
+            <div>
+                <img 
+                    src={`/icono_${categoryInfo.icon}.svg`} 
+                    alt="icon expense" 
+                    className="w-20"
+                />
+            </div>
+            <div className="flex-1 space-y-2">
+                <p className="text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
+                <p className="text-sm font-bold">{expense.expenseName}</p>
+                <p className="text-sm">{ formatDate( expense.date!.toString() )}</p>
+            </div>
+            <AmountDisplay 
+                amount={expense.amount} 
+            />
+        </div>
+    )
+}
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -4,3 +4,14 @@ export function formatCurrency(amount: number): string {
         currency: "USD",
     }).format(amount)
 }
+
+export function formatDate(dateStr: string) : string {
+    const dateObj = new Date(dateStr)
+    const options : Intl.DateTimeFormatOptions = {
+        year: "numeric", 
+        month: "long", 
+        weekday: "short", 
+        day: "numeric" 
+    }
+    return new Intl.DateTimeFormat("en-US", options).format(dateObj)
+}
