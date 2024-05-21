dbb624e48a5f42e085997aacc3970beec6392359
MmmarRTHa <m27842n@outlook.com>
2024-05-21 00:47:58 -0700

    - update: feature/expense_list_component - add swipeable library, add delete expense on swipe

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -4,6 +4,7 @@ import { formatDate } from '../helpers'
 import { Expense } from '../types'
 import AmountDisplay from './AmountDisplay'
 import { categories } from '../data/categories'
+import { useBudget } from '../hooks/useBudget'
 import "react-swipeable-list/dist/styles.css"
 
 type ExpenseDetailProps = {
@@ -11,25 +12,55 @@ type ExpenseDetailProps = {
 }
 
 export default function ExpenseDetail({expense} : ExpenseDetailProps) {
+    const { dispatch } = useBudget()
     const categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category)[0], [expense])
 
+    const leadingActions = () => (
+        <LeadingActions>
+            <SwipeAction
+                onClick={() => {}}
+            >
+                Update
+            </SwipeAction>
+        </LeadingActions>
+    )
+
+    const trailingActions = () => (
+        <TrailingActions>
+            <SwipeAction
+                onClick={() => dispatch({ type: 'remove-expense', payload: {id: expense.id} })}
+                destructive={true}
+            >
+                Delete
+            </SwipeAction>
+        </TrailingActions>
+    )
+
     return (
-        <div className="flex items-center w-11/12 gap-5 p-10 mx-auto border-b rounded-sm shadow md:max-w-3xl bg-gradient-to-r from-gray-200 to-gray-300 shadow-indigo-300">
-            <div>
-                <img 
-                    src={`/icono_${categoryInfo.icon}.svg`} 
-                    alt="icon expense" 
-                    className="w-20"
-                />
-            </div>
-            <div className="flex-1 space-y-2">
-                <p className="text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
-                <p className="text-sm font-bold">{expense.expenseName}</p>
-                <p className="text-sm">{ formatDate( expense.date!.toString() )}</p>
-            </div>
-            <AmountDisplay 
-                amount={expense.amount} 
-            />
-        </div>
+        <SwipeableList>
+            <SwipeableListItem
+                maxSwipe={1}
+                leadingActions={leadingActions()}
+                trailingActions={trailingActions()}
+            >
+                <div className="flex items-center w-11/12 gap-5 p-10 mx-auto border-b rounded-sm shadow md:max-w-3xl bg-gradient-to-r from-gray-200 to-gray-300 shadow-indigo-300">
+                    <div>
+                        <img 
+                            src={`/icono_${categoryInfo.icon}.svg`} 
+                            alt="icon expense" 
+                            className="w-20"
+                        />
+                    </div>
+                    <div className="flex-1 space-y-2">
+                        <p className="text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
+                        <p className="text-sm font-bold">{expense.expenseName}</p>
+                        <p className="text-sm">{ formatDate( expense.date!.toString() )}</p>
+                    </div>
+                    <AmountDisplay 
+                        amount={expense.amount} 
+                    />
+                </div>
+            </SwipeableListItem>
+        </SwipeableList>
     )
 }
