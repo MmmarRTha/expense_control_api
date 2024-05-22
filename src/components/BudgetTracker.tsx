3c71afa43c1b9cad57b7623f56e2fe055d39d773
MmmarRTHa <m27842n@outlook.com>
2024-05-21 19:46:38 -0700

    - update: feature/interactive_pie_chart - add interavtive pie chart

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,14 +1,26 @@
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
+import "react-circular-progressbar/dist/styles.css";
 
 export default function BudgetTracker() {
 
     const { state, totalExpenses, remainingBudget } = useBudget();
+    const percentage = +((totalExpenses / state.budget) * 100).toFixed(2)
     
     return (
         <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
             <div className="flex justify-center">
-                <img src="/grafico.jpg" alt="Expenses graphic" />
+                <CircularProgressbar 
+                    value={percentage}
+                    styles={buildStyles({
+                        textColor: percentage === 100 ? '#DC2626' : '#0284c7',
+                        pathColor: percentage === 100 ? '#DC2626' : '#DFFF00',
+                        trailColor: '#ADC2D1',
+                        textSize: 12
+                    })}
+                    text={`${percentage}% Spent`}
+                />
             </div>
 
             <div className="flex flex-col items-center justify-center gap-8">
@@ -29,7 +41,7 @@ export default function BudgetTracker() {
                 />
 
                 <AmountDisplay
-                    label="Total Expense"
+                    label="Total Spent"
                     amount={totalExpenses}
                 />
 
